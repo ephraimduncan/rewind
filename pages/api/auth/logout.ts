ba60f8b606f9cb8253f5ad121d6c74a58475db12
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-11-29 15:58:17 +0000

    fix(auth): Fix logout bug in cookies

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -2,7 +2,8 @@ import * as cookie from "cookies-next";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  cookie.deleteCookie("oauth2_token", { req, res });
+  cookie.deleteCookie("oauth2_access_token", { req, res });
+  cookie.deleteCookie("oauth2_refresh_token", { req, res });
 
   res.status(200).redirect("/");
 }
