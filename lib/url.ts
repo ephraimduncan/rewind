d0362b4474b3edc12ca39da5c286f007f0ba44ac
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-12-09 13:17:08 +0000

    Update build url

--- lib/url.ts
+++ lib/url.ts
@@ -2,15 +2,15 @@ import { NEXT_PUBLIC_VERCEL_URL, VERCEL_URL } from "./config";
 
 export function serverUrl() {
   if (VERCEL_URL) {
-    return `https://${VERCEL_URL}`;
+    return "https://rewind-zeta.vercel.app";
   }
 
   return "http://localhost:3000";
 }
 
 export function clientUrl() {
-  if (NEXT_PUBLIC_VERCEL_URL) {
-    return `https://${NEXT_PUBLIC_VERCEL_URL}`;
+  if (VERCEL_URL) {
+    return "https://rewind-zeta.vercel.app";
   }
 
   return "http://localhost:3000";
